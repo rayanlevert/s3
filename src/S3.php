be89a44fc4a6309139b990948f6ddb123071a45a
Rayan <rlevert@group-dis.com>
2023-10-04 11:42:51 +0200

    S3::fromArray() : ajout d'une méthode qui initialise l'instance via un array

--- src/S3.php
+++ src/S3.php
@@ -13,6 +13,29 @@ class S3
     protected \Aws\S3\S3Client $client;
 
     /**
+     * Initialise le client S3 avec les credentials, région et endpoint de l'API depuis un array
+     *
+     * @param array{key: string, secret: string, endpoint: string, region: string} $array
+     *
+     * @throws \UnexpectedValueException Si l'array comporte des valeurs incorrectes
+     */
+    public static function fromArray(array $array): self
+    {
+        $key        = $array['key'] ?? null;
+        $secret     = $array['secret'] ?? null;
+        $endpoint   = $array['endpoint'] ?? null;
+        $region     = $array['region'] ?? null;
+
+        if (!is_string($key) || !is_string($secret) || !is_string($endpoint) || !is_string($region)) {
+            throw new \UnexpectedValueException(
+                __METHOD__ . ' : key, secret, endpoint and region must be string values'
+            );
+        }
+
+        return new self($key, $secret, $endpoint, $region);
+    }
+
+    /**
      * Initialise le client S3 avec les credentials, région et endpoint de l'API
      *
      * @param string $key Access Key
--- tests/S3Test.php
+++ tests/S3Test.php
@@ -4,6 +4,7 @@ namespace DisDev\S3\Tests;
 
 use DisDev\S3\S3;
 use ReflectionProperty;
+use UnexpectedValueException;
 
 class S3Test extends TestCase
 {
@@ -27,5 +28,82 @@ class S3Test extends TestCase
         $this->assertSame('secret', $oS3Client->getCredentials()->wait()->getSecretKey());
 
         $this->assertSame('region', $oS3Client->getRegion());
+    }
+
+    /**
+     * @test ::fromArray()
+     */
+    public function testFromArray(): void
+    {
+        $oS3 = S3::fromArray([
+            'key'       => 'key',
+            'secret'    => 'secret',
+            'endpoint'  => 'https://endpoint.com',
+            'region'    => 'region'
+        ]);
+
+        $oS3Client = new ReflectionProperty($oS3, 'client');
+        $oS3Client->setAccessible(true);
+
+        /** @var \Aws\S3\S3Client $oS3Client */
+        $oS3Client = $oS3Client->getValue($oS3);
+
+        $this->assertSame('endpoint.com', $oS3Client->getEndpoint()->getHost());
+        $this->assertSame('https', $oS3Client->getEndpoint()->getScheme());
+
+        $this->assertSame('key', $oS3Client->getCredentials()->wait()->getAccessKeyId());
+        $this->assertSame('secret', $oS3Client->getCredentials()->wait()->getSecretKey());
+
+        $this->assertSame('region', $oS3Client->getRegion());
+    }
+
+    /**
+     * @test ::fromArray() empty array
+     */
+    public function testFromArrayEmptyArray(): void
+    {
+        $this->expectException(UnexpectedValueException::class);
+
+        S3::fromArray([]);
+    }
+
+    /**
+     * @test ::fromArray() not key entry
+     */
+    public function testFromArrayNotKey(): void
+    {
+        $this->expectException(UnexpectedValueException::class);
+
+        S3::fromArray(['secret' => 'secret', 'endpoint' => 'https://endpoint.com', 'region' => 'region']);
+    }
+
+    /**
+     * @test ::fromArray() not secret entry
+     */
+    public function testFromArrayNotSecret(): void
+    {
+        $this->expectException(UnexpectedValueException::class);
+
+        S3::fromArray(['key' => 'key', 'endpoint' => 'https://endpoint.com', 'region' => 'region']);
+    }
+
+    /**
+     * @test ::fromArray() not endpoint
+     */
+    public function testFromArrayNotEndpoint(): void
+    {
+        $this->expectException(UnexpectedValueException::class);
+
+        S3::fromArray(['key' => 'key', 'secret' => 'secret', 'region' => 'region']);
+    }
+
+    /**
+     * @test ::fromArray() not region
+     */
+    public function testFromArrayNotRegion(): void
+    {
+        $this->expectException(UnexpectedValueException::class);
+
+        S3::fromArray(['secret' => 'secret', 'key' => 'key', 'endpoint' => 'https://endpoint.com']);
     }
 }
