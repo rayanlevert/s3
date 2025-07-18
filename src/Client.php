abdcec23f528a08552140d587156981f61a37bd3
Rayan Levert <rayanlevert@msn.com>
2025-07-18 07:16:56 +0200

    Client : beginning of the class instantiating the cURL instance #9

---
+++ src/Client.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace RayanLevert\S3;
+
+use CurlHandle;
+
+use function curl_init;
+use function curl_setopt_array;
+use function filter_var;
+
+/** Client handling cURL requests */
+class Client
+{
+    private CurlHandle $curl;
+
+    /**
+     * @param string $endpoint Base URI URL of the S3 endpoint
+     *
+     * @throws Exception If the endpoint URL is invalid
+     */
+    public function __construct(public readonly string $endpoint)
+    {
+        if (!filter_var($this->endpoint, FILTER_VALIDATE_URL)) {
+            throw new Exception('Invalid endpoint URL: ' . $this->endpoint);
+        }
+
+        $this->curl = curl_init($this->endpoint);
+
+        curl_setopt_array($this->curl, [
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_MAXREDIRS      => 10
+        ]);
+    }
+}
---
+++ tests/ClientTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace RayanLevert\S3\Tests;
+
+use CurlHandle;
+use RayanLevert\S3\Exception;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+use RayanLevert\S3\Client;
+use PHPUnit\Framework\Attributes\Test;
+use ReflectionProperty;
+
+#[CoversClass(Client::class)]
+class ClientTest extends TestCase
+{
+    #[Test]
+    public function constructorException(): void
+    {
+        $this->expectException(Exception::class);
+
+        new Client('not a valid url');
+    }
+
+    #[Test]
+    public function constructor(): void
+    {
+        $client = new Client('http://localhost:9000');
+
+        $this->assertInstanceOf(Client::class, $client);
+        $this->assertSame('http://localhost:9000', $client->endpoint);
+
+        $oCurl = new ReflectionProperty($client, 'curl');
+
+        $this->assertInstanceOf(CurlHandle::class, $oCurl = $oCurl->getValue($client));
+        $this->assertSame('http://localhost:9000', curl_getinfo($oCurl)['url']);
+    }
+}
